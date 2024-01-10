dd6f678419f29364e5a0bdda0c3bd7f3f5a3ed2b
mr <malt.r@mail.de>
2024-01-10 15:29:45 +0100

    Test custom equals of callback adapter

--- dungeon/test/dsl/interpreter/TestDSLInterpreter.java
+++ dungeon/test/dsl/interpreter/TestDSLInterpreter.java
@@ -4931,4 +4931,67 @@ public class TestDSLInterpreter {
         String output = outputStream.toString();
         Assert.assertEquals("true" + System.lineSeparator(), output);
     }
+
+    @Test
+    public void testCallbackAdapterEquality() {
+        String program =
+            """
+            single_choice_task t1 {
+                description: "Task1",
+                answers: ["1", "2", "3"],
+                correct_answer_index: 2,
+                scenario_builder: build_scenario1
+            }
+
+            entity_type wizard_type {
+                draw_component {
+                    path: "character/wizard"
+                },
+                interaction_component{}
+            }
+
+            graph g {
+                t1
+            }
+
+            dungeon_config c {
+                dependency_graph: g
+            }
+
+            fn on_interaction(entity ent1, entity ent2) {
+                print("Hello");
+            }
+
+            fn build_scenario1(single_choice_task t) -> entity<><> {
+                var ret_set : entity<><>;
+                var room_set : entity<>;
+
+                var wizard1 : entity;
+                var wizard2 : entity;
+                wizard1 = instantiate(wizard_type);
+                wizard2 = instantiate(wizard_type);
+
+                wizard1.interaction_component.on_interaction = on_interaction;
+                wizard2.interaction_component.on_interaction = on_interaction;
+                print(wizard1.interaction_component.on_interaction == wizard2.interaction_component.on_interaction);
+
+                ret_set.add(room_set);
+                return ret_set;
+            }
+            """;
+
+        DSLInterpreter interpreter = new DSLInterpreter();
+        DungeonConfig config = (DungeonConfig) interpreter.getQuestConfig(program);
+
+        // print currently just prints to system.out, so we need to
+        // check the contents for the printed string
+        var outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        var task = config.dependencyGraph().nodeIterator().next().task();
+        var builtTask = (HashSet<HashSet<core.Entity>>) interpreter.buildTask(task).get();
+
+        String output = outputStream.toString();
+        Assert.assertEquals("true" + System.lineSeparator(), output);
+    }
 }
