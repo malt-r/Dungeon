330eacfac56d345d0830ade981e310f019d9f9a3
mr <malt.r@mail.de>
2024-01-10 16:28:58 +0100

    Test ListValue::equals

--- dungeon/test/dsl/interpreter/TestDSLInterpreter.java
+++ dungeon/test/dsl/interpreter/TestDSLInterpreter.java
@@ -5225,4 +5225,124 @@ public class TestDSLInterpreter {
         String output = outputStream.toString();
         Assert.assertEquals("true" + System.lineSeparator(), output);
     }
+
+    @Test
+    public void testListEqualsInteger() {
+        String program =
+            """
+            single_choice_task t1 {
+                description: "Task1",
+                answers: ["1", "2", "3"],
+                correct_answer_index: 2,
+                scenario_builder: build_scenario1
+            }
+
+            graph g {
+                t1
+            }
+
+            dungeon_config c {
+                dependency_graph: g
+            }
+
+            fn build_scenario1(single_choice_task t) -> entity<><> {
+                var ret_set : entity<><>;
+                var room_set : entity<>;
+
+                var list1 : int[];
+                var list2 : int[];
+                list1.add(1);
+                list1.add(2);
+                list1.add(3);
+                list2.add(1);
+                list2.add(2);
+                list2.add(3);
+                print(list1 == list2);
+
+                ret_set.add(room_set);
+                return ret_set;
+            }
+            """;
+
+        DSLInterpreter interpreter = new DSLInterpreter();
+        DungeonConfig config = (DungeonConfig) interpreter.getQuestConfig(program);
+
+        // print currently just prints to system.out, so we need to
+        // check the contents for the printed string
+        var outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        var task = config.dependencyGraph().nodeIterator().next().task();
+        var builtTask = (HashSet<HashSet<core.Entity>>) interpreter.buildTask(task).get();
+
+        String output = outputStream.toString();
+        Assert.assertEquals("true" + System.lineSeparator(), output);
+    }
+
+    @Test
+    public void testListEqualsAggregateValue() {
+        String program =
+            """
+            single_choice_task t1 {
+                description: "Task1",
+                answers: ["1", "2", "3"],
+                correct_answer_index: 2,
+                scenario_builder: build_scenario1
+            }
+
+            entity_type wizard_type {
+                draw_component {
+                    path: "character/wizard"
+                },
+                interaction_component{}
+            }
+
+            graph g {
+                t1
+            }
+
+            dungeon_config c {
+                dependency_graph: g
+            }
+
+            fn build_scenario1(single_choice_task t) -> entity<><> {
+                var ret_set : entity<><>;
+                var room_set : entity<>;
+
+                var list1 : entity[];
+                var list2 : entity[];
+                var wizard1 : entity;
+                wizard1 = instantiate(wizard_type);
+                var wizard2 : entity;
+                wizard2 = instantiate(wizard_type);
+                var wizard3 : entity;
+                wizard3 = instantiate(wizard_type);
+
+                list1.add(wizard1);
+                list1.add(wizard2);
+                list1.add(wizard3);
+                list2.add(wizard1);
+                list2.add(wizard2);
+                list2.add(wizard3);
+                print(list1 == list2);
+
+                ret_set.add(room_set);
+                return ret_set;
+            }
+            """;
+
+        DSLInterpreter interpreter = new DSLInterpreter();
+        DungeonConfig config = (DungeonConfig) interpreter.getQuestConfig(program);
+
+        // print currently just prints to system.out, so we need to
+        // check the contents for the printed string
+        var outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        var task = config.dependencyGraph().nodeIterator().next().task();
+        var builtTask = (HashSet<HashSet<core.Entity>>) interpreter.buildTask(task).get();
+
+        String output = outputStream.toString();
+        Assert.assertEquals("true" + System.lineSeparator(), output);
+    }
 }
